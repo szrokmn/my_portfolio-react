f8e5a3146557aec80d932c8089471811adb9dda9
Sezer Ökmen <szrokmn34@gmail.com>
2022-11-11 14:06:10 +0300

    add nav.jsx icons

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import "./Nav.css"
+import { AiOutlineHome, AiOutlineUser } from "react-icons/ai"
+import { BiBook, BiMessageSquareDetail } from "react-icons/bi"
+import { RiServiceLine } from "react-icons/ri"
+
 const Nav = () => {
   return (
-    <div>
-      Nav
-    </div>
+    <nav>
+      <a href="#"><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#service"><RiServiceLine/></a>
+      <a href="#contact"><BiMessageSquareDetail/></a>
+    </nav>
   )
 }
 
