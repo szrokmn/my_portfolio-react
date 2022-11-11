81a21de9ef1ce25ce07d2e844bae8526375621ab
Sezer Ökmen <szrokmn34@gmail.com>
2022-11-11 12:17:54 +0300

    CTA import

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import "./Header.css"
+import CTA from './CTA';
+
 const Header = () => {
   return (
     <div>
-      Header
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Steve - Mentor</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+      </div>
     </div>
   )
 }
