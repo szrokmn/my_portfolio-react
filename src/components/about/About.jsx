a9e2e0a1cb9d21f6b80c916c862f09b57b0f1f21
Sezer Ökmen <szrokmn34@gmail.com>
2022-11-11 16:54:42 +0300

    About.jsx finished

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
 import "./About.css"
+import ME from "../../assets/me-about.jpg"
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
   return (
     <section id='about'>
-      About
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+      <div className="about__contetnt">
+
+        <div className="about__cards">
+          <article className="about__card">
+            <FaAward className='about__icon'/>
+            <h5>Experience</h5>
+            <small>3+ Years Working</small>
+          </article>
+        </div>
+
+        <div className="about__cards">
+          <article className="about__card">
+            <FiUsers className='about__icon'/>
+            <h5>Clients</h5>
+            <small>200+ Worldwide</small>
+          </article>
+        </div>
+
+        <div className="about__cards">
+          <article className="about__card">
+            <VscFolderLibrary className='about__icon'/>
+            <h5>Projects</h5>
+            <small>Completed</small>
+          </article>
+        </div>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis, cum, inventore, exercitationem maiores quidem porro impedit ex voluptatibus corporis expedita minus vel nobis accusamus perspiciatis? Incidunt unde molestiae sequi at.</p>
+
+        <a href="#contact" className='btn btn-primary' >Let's Talk</a>
+
+      </div>
+
+      </div>
     </section>
   )
 }
