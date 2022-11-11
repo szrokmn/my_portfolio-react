0f1204ef567d062852c955bfbef118ce05212823
Sezer Ökmen <szrokmn34@gmail.com>
2022-11-11 13:24:18 +0300

    add Socialİcons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./Header.css"
 import CTA from './CTA';
+import ME from '../../assets/me.png'
+import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -10,6 +12,11 @@ const Header = () => {
         <h1>Steve - Mentor</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
+        <HeaderSocial/>
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a href="contact" className='scroll__down'>Scroll Down</a>
       </div>
     </div>
   )
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BsLinkedin } from "react-icons/bs"
+import { FaGithub, FaMedium } from "react-icons/fa"
+
+const HeaderSocials = () => {
+  return (
+    <div>
+        <a href="https://www.linkedin.com/in/sezer-%C3%B6kmen/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/szrokmn" target="_blank"><FaGithub/></a>
+        <a href="https://medium.com/@szrokmn34/about" target="_blank"><FaMedium/></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
