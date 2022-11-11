61753fe8455ef6fef80171ef32700582277257a8
Sezer Ökmen <szrokmn34@gmail.com>
2022-11-11 13:52:35 +0300

    header.css media querıes finished

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,7 +4,7 @@ import { FaGithub, FaMedium } from "react-icons/fa"
 
 const HeaderSocials = () => {
   return (
-    <div>
+    <div className='header__socials'>
         <a href="https://www.linkedin.com/in/sezer-%C3%B6kmen/" target="_blank"><BsLinkedin/></a>
         <a href="https://github.com/szrokmn" target="_blank"><FaGithub/></a>
         <a href="https://medium.com/@szrokmn34/about" target="_blank"><FaMedium/></a>
