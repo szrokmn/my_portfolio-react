2e037e23c6f16da737d435ef2b0dec29f496c388
Sezer Ökmen <szrokmn34@gmail.com>
2022-11-12 10:52:48 +0300

    add import images in Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import "./Portfolio.css"
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.jpg'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
-      Portfolio
+      <h5>My Recent Work</h5>
+      <h2>Potfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image"></div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/szrokmn" className='btn' target='_blank'>Github</a>
+          <a href="https://dribbble.com/" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+      </div>
     </section>
   )
 }
